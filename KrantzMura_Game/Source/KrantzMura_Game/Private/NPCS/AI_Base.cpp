ff51c86874792179499411e8527ff1b7d8989225
Davidpurtill1101011 <davidpurtill91@outlook.com>
2022-01-26 02:13:35 +0000

    Revert "Merge pull request #39 from Davidpurtill1101011/AI-refactor"
    This reverts commit 4628c6e1e7bbd69a68f31f8ac9b5c076922775f6, reversing
    changes made to 44b5752f959936d8e03a82cfe229b1c0c0b3dde4.

--- KrantzMura_Game/Source/KrantzMura_Game/Private/NPCS/AI_Base.cpp
+++ KrantzMura_Game/Source/KrantzMura_Game/Private/NPCS/AI_Base.cpp
@@ -1,7 +1,9 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
+#include <NPCS/GuardAIController.h>
+#include <NPCS/Waypoint.h>
 #include "NPCS/AI_Base.h"
-#include <GameFramework/CharacterMovementComponent.h>
+#include <Runtime/Engine/Classes/Kismet/GameplayStatics.h>
 
 // Sets default values
 AAI_Base::AAI_Base()
@@ -9,8 +11,6 @@ AAI_Base::AAI_Base()
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
-	GetCharacterMovement()->bOrientRotationToMovement = true;// lets the character to turn in the way it is walking
-	GetCharacterMovement()->RotationRate = FRotator(0.0f, 600.0f, 0.0f);// turn rate of the AI
 }
 
 // Called when the game starts or when spawned
@@ -18,7 +18,8 @@ void AAI_Base::BeginPlay()
 {
 	Super::BeginPlay();
 	
-	
+	UGameplayStatics::GetAllActorsOfClass(GetWorld(), AWaypoint::StaticClass(), WaypointsArray);
+	MoveToWaypoints();
 }
 
 // Called every frame
@@ -34,3 +35,34 @@ void AAI_Base::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 
 }
+
+void AAI_Base::MoveToWaypoints()
+{
+	// this function lets the AI move to different waypoints set out in the area 
+	// to patrol the set out spot.
+
+	AGuardAIController* GuardAIController = Cast<AGuardAIController>(GetController());
+
+	if (GuardAIController) {// checking to see if the cast was good
+		if (CurrentWaypoint <= WaypointsArray.Num()) {
+			for (AActor* Waypoints : WaypointsArray) 
+			{
+				AWaypoint* WaypointItr = Cast<AWaypoint>(Waypoints);
+				if (WaypointItr) {
+					if (WaypointItr->GetWaypointOrder() == CurrentWaypoint) {
+						GuardAIController->MoveToActor(WaypointItr, 0.1f, false);
+						CurrentWaypoint++;
+						if (CurrentWaypoint > WaypointsArray.Num()) {
+							CurrentWaypoint = 1;
+							break;
+						}
+						break;
+					}
+					
+				}
+			}
+		}
+	}
+
+}
+
--- KrantzMura_Game/Source/KrantzMura_Game/Public/NPCS/GuardAIController.h
+++ KrantzMura_Game/Source/KrantzMura_Game/Public/NPCS/GuardAIController.h
@@ -14,34 +14,6 @@ class KRANTZMURA_GAME_API AGuardAIController : public AAIController
 {
 	GENERATED_BODY()
 
-public:
-	AGuardAIController();
-
-	virtual void BeginPlay() override;
-
-	virtual void OnPossess(APawn* MyPawn) override;
-
-	//virtual void OnUnPossess();
-
-	virtual void Tick(float DeltaSeconds) override;
-
-	virtual FRotator GetControlRotation() const override;
-
-	UFUNCTION()
-		void OnPawnDetected(const TArray<AActor*> &DetectedPawns);// this function lets the AI pickup on objects that have entered their vicinity
-	
-	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = AI)
-		float DetectSightRadius = 500.0f;// detection area
-
-	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = AI)
-		float SightAge = 5.0f;// how long the AI keeps the object that is found in memory
-
-	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = AI)
-		float LoseSightradius = DetectSightRadius + 25.0f;// the distance the Player needs to be away for the AI to lose sight of them
-
-	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = AI)
-		float FieldOfView = 90.0f;// vision cone for the AI to spot the Player.
-
-	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = AI)
-		class UAISenseConfig_Sight* SightConfig;
+	// this function lets the ai do the loop of the patrol again
+	virtual void OnMoveCompleted(FAIRequestID ReqID, const FPathFollowingResult& Res) override;
 };
