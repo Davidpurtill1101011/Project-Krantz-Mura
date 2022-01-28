24723821a31bb5caa5a4ba97df3244b5cc19f7c5
Davidpurtill1101011 <davidpurtill91@outlook.com>
2022-01-28 22:02:40 +0000

    Added some code to make the AI detect the player and updated boiler code from last night

--- KrantzMura_Game/Source/KrantzMura_Game/AI_Patrol_Character.cpp
+++ KrantzMura_Game/Source/KrantzMura_Game/AI_Patrol_Character.cpp
@@ -2,6 +2,7 @@
 
 
 #include "AI_Patrol_Character.h"
+#include <GameFramework/CharacterMovementComponent.h>
 
 // Sets default values
 AAI_Patrol_Character::AAI_Patrol_Character()
@@ -9,6 +10,8 @@ AAI_Patrol_Character::AAI_Patrol_Character()
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	GetCharacterMovement()->bOrientRotationToMovement = true;// character to turn itself in the direction of the motion(Face the way its wanting to walk)
+	GetCharacterMovement()->RotationRate = FRotator(0.0f, 600.0f, 0.0f);// the speed in which it rotates at on the Y Axis
 }
 
 // Called when the game starts or when spawned
