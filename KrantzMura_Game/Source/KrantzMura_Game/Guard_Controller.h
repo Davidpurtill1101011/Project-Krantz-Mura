b5cd7072754b8e953faf7fdaea6668d929e2ac53
Davidpurtill1101011 <davidpurtill91@outlook.com>
2022-01-29 02:13:06 +0000

    Controller added and AiController removed

---
+++ KrantzMura_Game/Source/KrantzMura_Game/Guard_Controller.h
@@ -0,0 +1,18 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include <AIController.h>
+#include "GameFramework/Controller.h"
+#include "Guard_Controller.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class KRANTZMURA_GAME_API AGuard_Controller : public AController
+{
+	GENERATED_BODY()
+	
+};
