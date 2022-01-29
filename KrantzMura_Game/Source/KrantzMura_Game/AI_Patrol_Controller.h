1402fc71ea2f3b18f2e8071a3a5a4e52749fb106
Davidpurtill1101011 <davidpurtill91@outlook.com>
2022-01-29 01:53:58 +0000

    Added ai controller for guard

---
+++ KrantzMura_Game/Source/KrantzMura_Game/AI_Patrol_Controller.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AIController.h"
+#include "AI_Patrol_Controller.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class KRANTZMURA_GAME_API AAI_Patrol_Controller : public AAIController
+{
+	GENERATED_BODY()
+	
+};
