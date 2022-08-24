2e0e06f32856a8ab93ae62fdeaf7fdeb655fb947
Davidpurtill1101011 <davidpurtill91@outlook.com>
2022-08-24 03:49:58 +0100

    Set up weapon class and have some attacks setup on it, Also some collisions set up

---
+++ KrantzMura_Game/Source/KrantzMura_Game/Public/WeaponBase.h
@@ -0,0 +1,36 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include <Components/BoxComponent.h>
+#include "WeaponBase.generated.h"
+
+UCLASS()
+class KRANTZMURA_GAME_API AWeaponBase : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AWeaponBase();
+
+	UPROPERTY(EditAnywhere)
+		class USkeletalMeshComponent* WeaponMesh;
+
+	UPROPERTY(EditAnywhere)
+		class UAnimationAsset* SlashAnimation;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadonly, Category = "Collision")
+		class UBoxComponent* SwordCollisionBox;
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	void Slash();
+	void StopSlash();
+
+};
